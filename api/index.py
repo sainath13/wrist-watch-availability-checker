ab6c900c95cc266eff65eeec45eb70c544b3ed3f
sainathl <latkarsainath@gmail.com>
2024-03-25 11:46:19 +0530

    [Add] hmt link

--- api/index.py
+++ api/index.py
@@ -1,4 +1,21 @@
 from http.server import BaseHTTPRequestHandler
+import requests
+
+url = "https://www.hmtwatches.in/product_details?id=eyJpdiI6Ii9weGRHYk02Qmh6WGViYWFjVWY5ZEE9PSIsInZhbHVlIjoiS3BXOVlQM1RnblBWdkpHcEZFeXMxQT09IiwibWFjIjoiZTc4OWQ3YzMzZjQ1ZTJkOGI5NWZhNTA2ZTc2Mzc5NWI2YmJiOWYwOTM5NTBkMWE1MDdmOTcxYTI2MjliNTcwZSIsInRhZyI6IiJ9"
+
+def check_availability(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            if "Out Of Stock" not in response.text:
+                return "The watch is available!"
+            else:
+                return "The watch is out of stock."
+        else:
+            return "Failed to fetch webpage. Status code:" + response.status_code
+    except Exception as e:
+        return "An error occurred:"+ str(e)
+
 
 class handler(BaseHTTPRequestHandler):
 
@@ -6,5 +23,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        strToPrint = check_availability(url)
+        self.wfile.write(strToPrint.encode('utf-8'))
         return
