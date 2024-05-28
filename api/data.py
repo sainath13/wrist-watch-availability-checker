cef99f7dbcfbfa0171215090acdc187e1b73567f
sainathl <latkarsainath@gmail.com>
2024-05-28 23:08:21 +0530

    [Add] change data

---
+++ api/data.py
@@ -0,0 +1,43 @@
+# api/data.py
+import json
+from http.server import BaseHTTPRequestHandler
+
+journal_entries = []
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data)
+        
+        JournalEntry = data.get('JournalEntry')
+        Mood = data.get('Mood')
+
+        if not JournalEntry or not Mood:
+            self.send_response(400)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({'error': 'JournalEntry and Mood are required.'}).encode('utf-8'))
+            return
+
+        entry = {
+            'id': len(journal_entries) + 1,
+            'JournalEntry': JournalEntry,
+            'Mood': Mood,
+            'createdAt': str(datetime.datetime.now())
+        }
+
+        journal_entries.append(entry)
+        
+        self.send_response(201)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({'message': 'Journal entry added.', 'entry': entry}).encode('utf-8'))
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'POST')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+
---
+++ api/getData.py
@@ -0,0 +1,19 @@
+# api/getData.py
+import json
+from http.server import BaseHTTPRequestHandler
+from .data import journal_entries
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(journal_entries).encode('utf-8'))
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+
