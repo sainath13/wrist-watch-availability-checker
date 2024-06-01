c57ca1ffe3f868f177b441804785034e78e0f649
sainathl <latkarsainath@gmail.com>
2024-06-01 19:57:55 +0530

    [Add] put to db

--- api/data.py
+++ api/data.py
@@ -2,6 +2,12 @@
 import json
 import datetime
 from http.server import BaseHTTPRequestHandler
+from textblob import TextBlob
+from supabase import create_client, Client
+
+url: str = "https://eugmxxtzlcdlrnosbowy.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1Z214eHR6bGNkbHJub3Nib3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwMDQzOTUsImV4cCI6MjAzMjU4MDM5NX0.TsfuVI_CDiXuiObg6GlToOlp37yGtWTLu3opXv_bB34"
+supabase: Client = create_client(url, key)
 
 journal_entries = []
 
@@ -13,7 +19,10 @@ class handler(BaseHTTPRequestHandler):
         
         JournalEntry = data.get('JournalEntry')
         Mood = data.get('Mood')
-
+        sentiment = TextBlob(JournalEntry).sentiment
+        polarity = sentiment.polarity
+        data, count = supabase.table('countries').insert({"mood": Mood, "mood_rating" : polarity, "journal_text" : JournalEntry }).execute()
+        
         if not JournalEntry or not Mood:
             self.send_response(400)
             self.send_header('Content-type', 'application/json')
@@ -25,6 +34,7 @@ class handler(BaseHTTPRequestHandler):
             'id': len(journal_entries) + 1,
             'JournalEntry': JournalEntry,
             'Mood': Mood,
+            'Polarity' : polarity,
             'createdAt': str(datetime.datetime.now())
         }
 
